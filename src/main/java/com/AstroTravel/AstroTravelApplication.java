54c830c220bcb9866ae654d7e1c13d1952a3313f
Bhupati Gudla <bhupatigudla00@gmail.com>
2025-06-15 19:59:03 +0530

    Initial Commit

---
+++ src/main/java/com/AstroTravel/AstroTravelApplication.java
@@ -0,0 +1,13 @@
+package com.AstroTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AstroTravel/AstroTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AstroTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
